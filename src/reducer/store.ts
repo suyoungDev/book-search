1f181962e28aa6ea91a80890e79ad2ee58f87af8
mocha <syda07@gmail.com>
2021-05-04 16:56:45 +0900

    ➕ redux-devtools 적용

--- src/reducer/store.ts
+++ src/reducer/store.ts
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './index';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export type RootReducerType = ReturnType<typeof rootReducer>;
 
