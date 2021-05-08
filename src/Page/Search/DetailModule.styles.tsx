ec88dc4828904b0cbb28c3a347ab7dc3cdfa77a8
mocha <syda07@gmail.com>
2021-05-08 16:24:57 +0900

    💄 책소개란 좀더 자세히

--- src/Page/Search/DetailModule.styles.tsx
+++ src/Page/Search/DetailModule.styles.tsx
@@ -14,3 +14,55 @@ export const StarContainer = styled.div`
   justify-content: center;
   margin: 0rem 0 1.2rem 0;
 `;
+
+export const Title = styled.div`
+  margin: 2rem 0;
+  color: ${(props) => props.theme.colors.primary50};
+`;
+
+export const Link = styled.div`
+  a {
+    color: ${(props) => props.theme.colors.primary50};
+    font-size: 1.2rem;
+    text-decoration: none;
+    :hover {
+      color: ${(props) => props.theme.colors.primary60};
+    }
+  }
+`;
+
+export const SaveComment = styled.div`
+  background-color: ${(props) => props.theme.colors.primary50};
+  color: white;
+  font-weight: bold;
+  padding: 0.7em 1.5em;
+  border-radius: 2rem;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${(props) => props.theme.colors.primary60};
+  }
+`;
+
+export const Bar = styled.div`
+  height: 100%;
+  width: 1px;
+  border-right: 1px solid ${(props) => props.theme.colors.ink30};
+`;
+
+export const Price = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.5rem 0;
+  p {
+    :first-of-type {
+      font-weight: bold;
+      font-size: 1.1rem;
+      color: ${(props) => props.theme.colors.primary50};
+    }
+    :not(:first-of-type) {
+      font-size: 0.8rem;
+    }
+  }
+`;
--- src/Page/Search/DetailModule.tsx
+++ src/Page/Search/DetailModule.tsx
@@ -7,16 +7,29 @@ import {
   AuthorDetail,
   DescriptionDetail,
 } from './SearchModule.styles';
-import { Container, StarContainer } from './DetailModule.styles';
+import {
+  Container,
+  SaveComment,
+  Link,
+  Title,
+  StarContainer,
+  Bar,
+  Price,
+} from './DetailModule.styles';
 import StarBox from './StarBox';
+import { Row } from '../../Components/Row';
 
 const DetailModule: React.FC = () => {
   const { data } = useSelector((state: RootReducerType) => state.detailReducer);
+  console.log(data);
 
   if (!data) return <div>업슴</div>;
 
   return (
     <Container>
+      <Title>
+        <h2>책 소개</h2>
+      </Title>
       <ImgDetail source={data.image} />
       <TitleBook>
         <p dangerouslySetInnerHTML={{ __html: data?.title }} />
@@ -30,6 +43,30 @@ const DetailModule: React.FC = () => {
       <DescriptionDetail>
         <p dangerouslySetInnerHTML={{ __html: data?.description }} />
       </DescriptionDetail>
+      <Row className='al-ct js-sb'>
+        <Price>
+          <p>{data.price}</p>
+          <p>가격</p>
+        </Price>
+        <Bar />
+        <Price>
+          <p>{data.publisher}</p>
+          <p>출판사</p>
+        </Price>
+        <Bar />
+        <Price>
+          <p>{data.pubdate}</p>
+          <p>출판일</p>
+        </Price>
+      </Row>
+      <Row className='al-ct js-sb'>
+        <Link>
+          <a href={data.link} type='text/html' target='_blank'>
+            사러가기
+          </a>
+        </Link>
+        <SaveComment>감상평 남기기</SaveComment>
+      </Row>
     </Container>
   );
 };
