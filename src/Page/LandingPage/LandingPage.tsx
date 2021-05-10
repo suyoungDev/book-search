022afbd42c40b643ea41fb6a5d6e68e085f824f5
mocha <syda07@gmail.com>
2021-05-10 11:18:23 +0900

    ✨ 404page생성

--- src/Page/LandingPage/LandingPage.tsx
+++ src/Page/LandingPage/LandingPage.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route, Switch } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 
 import Title from './Section/Title';
 import LandingContainer from './Section/LandingContainer';
 import LandingResultContainer from './Section/LandingResultContainer';
 import RecordPage from '../RecordPage/RecordPage';
+import NotFound from '../NotFound/NotFound';
 
 const Main = styled.main`
   width: 100%;
 `;
 
-const LandingPage: React.FC = () => {
+const LandingPage = () => {
   return (
     <Main>
       <Title />
       <Switch>
         <Route exact path='/' component={LandingContainer} />
-        <Route exact path='/search' component={LandingResultContainer} />
+        <Route exact path='/search/:id' component={LandingResultContainer} />
         <Route exact path='/record' component={RecordPage} />
+        <Route exact path='/404' component={NotFound} />
+        <Redirect to='/404' />
       </Switch>
     </Main>
   );
---
+++ src/Page/NotFound/NotFound.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div>찾는 페이지가 없습니다.</div>;
+};
+
+export default NotFound;
