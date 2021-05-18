b1d505b2394c141a89f2aeed3fc339d28d3f569d
mocha <syda07@gmail.com>
2021-05-18 17:31:03 +0900

    💄 모바일 용 NavBar 버거 생성

--- src/Page/Header/Header.tsx
+++ src/Page/Header/Header.tsx
@@ -4,13 +4,17 @@ import Nav from './Nav';
 import Info from './Info';
 import { Props } from './Toggle';
 import { LeftWrapper } from '../../Components/SideWrapper';
+import Burger from './Burger';
 
 const Header: React.FC<Props> = ({ toggle }) => {
   return (
-    <LeftWrapper>
-      <Nav />
-      <Info toggle={toggle} />
-    </LeftWrapper>
+    <>
+      <LeftWrapper>
+        <Nav />
+        <Info toggle={toggle} />
+        <Burger />
+      </LeftWrapper>
+    </>
   );
 };
 
